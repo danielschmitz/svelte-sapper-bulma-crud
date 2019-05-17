d6d730d59842ee9b2e346e283876ec26f2ec386f
Daniel Schmitz <danieljfa@gmail.com>
2019-05-17 16:51:52 -0300

    feat: categories with action buttons

--- src/routes/categories.svelte
+++ src/routes/categories.svelte
@@ -17,6 +17,10 @@
       `🚨 Catastrofic failure 🚨 ${result.status} ${result.statusText}`
     );
   }
+
+  function onItemClick(item) {
+    console.log("onItemClick", item);
+  }
 </script>
 
 <svelte:head>
@@ -31,11 +35,12 @@
       <progress class="progress is-small is-primary" max="100">15%</progress>
     {:then list}
 
-      <table class="table is-striped">
+      <table class="table is-striped is-hoverable">
         <thead>
           <tr>
             <th>Name</th>
             <th>Description</th>
+            <th width="100px">Actions</th>
           </tr>
         </thead>
         <tbody>
@@ -43,6 +48,19 @@
             <tr>
               <td>{item.name}</td>
               <td>{item.description}</td>
+              <td>
+                <a href="javascript;" on:click={()=>onItemClick(item)}>
+                  <span class="icon is-small">
+                    <i class="fas fa-edit" />
+                  </span>
+                </a>
+                &nbsp;
+                 <a href="javascript;" on:click={()=>onDeleteClick(item)}>
+                  <span class="icon is-small">
+                    <i class="fas fa-trash" />
+                  </span>
+                </a>
+              </td>
             </tr>
           {/each}
         </tbody>
@@ -53,3 +71,5 @@
 
   </div>
 </Card>
+
+
